e7a127f8a8ea08c003b13c0245c56bb78b5ebd8b
Robbie Stamp <stamprobbie@gmail.com>
2023-08-22 17:29:20 +0100

    adding reserve to the update method so we can update whether books have been reserved or not

--- server/models/Book.js
+++ server/models/Book.js
@@ -35,8 +35,8 @@ class Book {
 }
 
 async update(data) {
-  const { name: name, author: author, genre: genre } = data
-  const response = await db.query('UPDATE books SET name = $1, author = $2, genre = $3 WHERE id = $4 RETURNING *', [name, author, genre, this.id])
+  const { name: name, author: author, genre: genre, reserved: reserved } = data
+  const response = await db.query('UPDATE books SET name = $1, author = $2, genre = $3 WHERE id = $4 RETURNING *', [name, author, genre, reserved, this.id])
 
   if (response.rows.length != 1) {
       throw new Error('Unable to update book.')
