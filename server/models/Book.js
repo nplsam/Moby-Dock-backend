e3a534c4252068cabedb3849ca3aa0a6b29418ff
Stephanie <stephf202@gmail.com>
2023-08-22 16:45:52 +0100

    add reserved

--- server/models/Book.js
+++ server/models/Book.js
@@ -6,6 +6,7 @@ class Book {
     this.name = data.name
     this.author = data.author
     this.genre = data.genre
+    this.reserved = data.reserved
   }
 
   static async getAll() {
@@ -25,16 +26,16 @@ class Book {
   }
 
   static async create(data) {
-    const { name: name, author: author, genre: genre } = data
-    const response = await db.query('INSERT INTO books (name, author, genre) VALUES ($1, $2, $3) RETURNING *', [name, author, genre]);
+    const { name: name, author: author, genre: genre, reserved: reserved } = data
+    const response = await db.query('INSERT INTO books (name, author, genre, reserved) VALUES ($1, $2, $3, $4) RETURNING *', [name, author, genre, reserved]);
     const bookId = response.rows[0].id;
     const newBook = await Book.getOneById(bookId);
     return newBook;
 }
 
 async update(data) {
-  const { name: name, author: author, genre: genre } = data
-  const response = await db.query('UPDATE books SET name = $1, author = $2, genre = $3 WHERE id = $4 RETURNING *', [name, author, genre, this.id])
+  const { name: name, author: author, genre: genre, reserved: reserved } = data
+  const response = await db.query('UPDATE books SET name = $1, author = $2, genre = $3, reserved = $4 WHERE id = $5 RETURNING *', [name, author, genre, reserved, this.id])
 
   if (response.rows.length != 1) {
       throw new Error('Unable to update book.')
