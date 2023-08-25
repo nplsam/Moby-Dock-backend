5e355e320175973edcb854cb5195c10aa0a33cf5
Stephanie <stephf202@gmail.com>
2023-08-25 12:02:54 +0100

    uncommented out test for create

--- server/__tests__/models/ReservedBook.spec.js
+++ server/__tests__/models/ReservedBook.spec.js
@@ -59,17 +59,17 @@ describe('getOneById', () => {
 })
 
 describe('create', () => {
-  // it('should create reserved book with data', async() => {
-  //   let bookData = { book_id: 3, name: 'Pride and Prejudice', pick_up_by: '23/08/2023'}
-  //   jest.spyOn(db, 'query')
-  //     .mockResolvedValueOnce({ rows: [{...bookData, reserved_id: 1 }] })
+  it('should create reserved book with data', async() => {
+    let bookData = { book_id: 3, name: 'Pride and Prejudice', pick_up_by: '23/08/2023'}
+    jest.spyOn(db, 'query')
+      .mockResolvedValueOnce({ rows: [{...bookData, reserved_id: 1 }] })
 
-  //     const result = await ReservedBook.create(bookData)
-  //     expect(result).toHaveProperty('reserved_id')
-  //     expect(result).toHaveProperty('book_id')
-  //     expect(result).toHaveProperty('name')
-  //     expect(result).toHaveProperty('pick_up_by')
-  // })
+      const result = await ReservedBook.create(bookData)
+      expect(result).toHaveProperty('reserved_id')
+      expect(result).toHaveProperty('book_id')
+      expect(result).toHaveProperty('name')
+      expect(result).toHaveProperty('pick_up_by')
+  })
 
   it('should throw an Error on db query error', async () => {
     jest.spyOn(db, 'query').mockRejectedValue(new Error('Book id missing'))
